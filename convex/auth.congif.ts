5a98ffa6808cdcf01a11c0c04f9cc3c42ecf6afc
hamza bouyoussef <hamza8pro@gmail.com>
2024-10-17 18:08:11 +0100

    add configuration of clerk / add configuration of clerk/Convex / create middlere / add functions create / update delete user

---
+++ convex/auth.congif.ts
@@ -0,0 +1,10 @@
+
+const authConfig = {
+    providers: [
+        {
+            domain: "https://ultimate-quagga-15.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+export default authConfig;
